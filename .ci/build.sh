493f83bfec34ae389838f69532e63a6407c83947
jkoenig <julian.koenig@js-soft.com>
2021-09-24 09:58:01 +0200

    chore: stream lerna run commands

--- .ci/build.sh
+++ .ci/build.sh
@@ -4,6 +4,6 @@ set -x
 npm ci
 npx lerna bootstrap
 npm run lint:prettier
-npx lerna run license-check
+npx lerna run license-check --stream
 npx lerna run build --stream
-npm run lint:eslint
+npm run lint:eslint --stream
