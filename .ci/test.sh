93297cd4f1a51b4f54f5d98a8223f5462e1ca719
jkoenig <julian.koenig@js-soft.com>
2023-03-14 15:42:39 +0100

    ci: test on ferretdb

--- .ci/test.sh
+++ .ci/test.sh
@@ -1,5 +1,8 @@
 set -e
 set -x
 
-export CONNECTION_STRING="mongodb://127.0.0.1:27017/?readPreference=primary&appname=Core&ssl=false"
+export CONNECTION_STRING="mongodb://127.0.0.1:27017"
+npm run test:ci --workspaces
+
+export CONNECTION_STRING="mongodb://127.0.0.1:27018"
 npm run test:ci --workspaces
