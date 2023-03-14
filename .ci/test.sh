ac8c69701da618107fd7b1c98c81961a165b439d
jkoenig <julian.koenig@js-soft.com>
2023-03-14 15:49:30 +0100

    chore: only test if present

--- .ci/test.sh
+++ .ci/test.sh
@@ -2,7 +2,7 @@ set -e
 set -x
 
 export CONNECTION_STRING="mongodb://127.0.0.1:27017"
-npm run test:ci --workspaces
+npm run test:ci --workspaces --if-present
 
 export CONNECTION_STRING="mongodb://127.0.0.1:27018"
-npm run test:ci --workspaces
+npm run test:ci --workspaces --if-present
