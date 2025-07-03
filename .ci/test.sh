062a9ff4f94a6b9d038c99e29daba9148c5f1841
Julian König <julian.koenig@js-soft.com>
2025-07-03 11:19:56 +0200

    ci: actually fix the test pipeline

--- .ci/test.sh
+++ .ci/test.sh
@@ -4,5 +4,5 @@ set -x
 export CONNECTION_STRING="mongodb://127.0.0.1:27017"
 npm run test:ci --workspaces --if-present
 
-export CONNECTION_STRING="mongodb://username:password@127.0.0.1:27018"
+export CONNECTION_STRING="mongodb://127.0.0.1:27018"
 npm run test:ci --workspaces --if-present
