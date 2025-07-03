0984e17a2e9b5b1eb44e3f59d5b4b1161e2b498b
Julian König <julian.koenig@js-soft.com>
2025-07-03 14:17:51 +0200

    chore: simplfy

--- packages/loki/src/LokiJsCollectionProvider.ts
+++ packages/loki/src/LokiJsCollectionProvider.ts
@@ -11,10 +11,8 @@ export class LokiJsCollectionProvider implements IDatabaseCollectionProvider {
     private getLokidbCollection(name: string) {
         let collection = this.db.getCollection(name);
 
-        // eslint-disable-next-line @typescript-eslint/no-unnecessary-condition, @typescript-eslint/prefer-nullish-coalescing
-        if (collection === null) {
-            collection = this.db.addCollection(name);
-        }
+        // eslint-disable-next-line @typescript-eslint/no-unnecessary-condition
+        collection ??= this.db.addCollection(name);
 
         return collection;
     }
